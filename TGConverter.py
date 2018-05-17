69edd0c851612d769375fca1f499c8f2f7e7fe68
ChrisMiuchiz <chris000005@gmail.com>
2018-05-17 19:55:10 -0400

    Add files via upload

---
+++ TGConverter.py
@@ -0,0 +1,92 @@
+# ChrisMiuchiz
+# Requires Python 3.6+
+# This tool converts AW 6 telegram files into more usable formats
+from struct import *
+from Telegram import *
+import sys
+  
+def MakeTelegrams(datfile, idxfile):
+    with open(idxfile, 'rb') as f:
+        idx = f.read()
+    with open(datfile, 'rb') as f:
+        dat = f.read()
+
+    IDXLength, _, IDXEnd = unpack('<III', idx[:12])
+    IDXStart, = unpack('<I', idx[0x48:0x48+4])
+
+    telegrams = []
+    blockStart = IDXStart
+    while True:
+        #AW's dat and idx formats are like linked lists of arrays of whatever struct they're storing.
+        forward, backward, entrycount, entrieslength = unpack('<IIHH', idx[blockStart:blockStart+12])
+        if entrycount == 0 or entrieslength == 0:
+            break
+        ENTRY_LENGTH = entrieslength // entrycount
+
+        for i in range(entrycount):
+            entryStart = blockStart + ENTRY_LENGTH * i
+            
+            datAddress, sentTimestamp, localID = unpack('<III', idx[entryStart+18: entryStart + 30])
+            
+            usernamestart = entryStart + 0x1E
+            username = idx[usernamestart : usernamestart+32]
+            for i in range(0, len(username), 2):
+                if username[i:i+2] == b'\x00\x00':
+                    username = username[:i]
+                    break
+            username = username.decode('UTF-16')
+            
+            datEntryLength, = unpack('<I', dat[datAddress+2 : datAddress+6])
+            
+            tgtype = dat[datAddress + 0xA]
+
+            messageStart = datAddress + 0xB
+            message = dat[messageStart : datAddress+datEntryLength]
+
+            #Stop message at first NULL NULL
+            for i in range(0, len(message), 2):
+                if message[i:i+2] == b'\x00\x00':
+                    message = message[:i]
+                    break
+            message = message.decode('UTF-16')
+            
+            telegrams.append(Telegram(username, localID, message, sentTimestamp, tgtype))
+            
+        if forward == 0x00000000:
+            break
+        blockStart = forward
+
+        
+    return telegrams
+
+def AskForArgs():
+    dat = input('telegram5.dat file: ').strip('"')
+    idx = input('telegram5.idx file: ').strip('"')
+    otype = input('Output type (text | csv): ').strip('"')
+    output = input('Output file: ').strip('"')
+    return (dat, idx, otype, output)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        dat, idx, otype, output = AskForArgs()
+    elif len(sys.argv) == 5:
+        dat, idx, otype, output = sys.argv[1:]
+    else:
+        print('USAGE: TGConverter.py <telegram5.dat> <telegram5.idx> <text | csv> <output file>')
+        exit()
+     
+    if otype.lower() == 'text':
+        func = OutputText
+    elif otype.lower() == 'csv':
+        func = OutputCSV
+    else:
+        print(f'Unknown type: {otype}')
+        exit()
+
+    telegrams = MakeTelegrams(dat, idx)
+        
+    with open(output, 'wb') as f:
+        f.write(func(telegrams).encode('UTF-16'))
+    
+    
+    
---
+++ Telegram.py
@@ -0,0 +1,46 @@
+import datetime
+OUTGOING = 2
+INCOMING_READ = 1
+INCOMING_UNREAD = 0
+class Telegram():
+    def __init__(self, username, localID, message, timestamp, telegramType):
+        self.username = username
+        self.localID = localID
+        self.message = message
+        self.timestamp = timestamp
+        self.date = datetime.datetime.fromtimestamp(timestamp).strftime('%Y/%m/%d')
+        self.time = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M:%S')
+        self.type = telegramType
+    def MakeText(self):
+        if self.type == OUTGOING:
+            return f'Type: Outgoing\tFrom: ({self.localID})\tTo: {self.username}\tDate: {self.date} {self.time}\tMessage: {self.message}'
+        elif self.type == INCOMING_READ:
+            return f'Type: Incoming, read\tFrom: {self.username}\tTo: ({self.localID})\tDate: {self.date} {self.time}\tMessage: {self.message}'
+        elif self.type == INCOMING_UNREAD:
+            return f'Type: Incoming, unread\tFrom: {self.username}\tTo: ({self.localID})\tDate: {self.date} {self.time}\tMessage: {self.message}'
+        else:
+            return ''
+    def MakeCSV(self):
+        if self.type == OUTGOING:
+            return f'"Outgoing","({self.localID})","{self.username}","{self.date} {self.time}","{self.message}"'
+        elif self.type == INCOMING_READ:
+            return f'"Incoming, read","{self.username}","({self.localID})","{self.date} {self.time}","{self.message}"'
+        elif self.type == INCOMING_UNREAD:
+            return f'"Incoming, unread","{self.username}","({self.localID})","{self.date} {self.time}","{self.message}"'
+        else:
+            return ''
+            
+
+def OutputText(telegrams):
+    lines = []
+    for telegram in sorted(telegrams, key=lambda x: x.timestamp):
+        lines.append(telegram.MakeText())
+    return '\n'.join(lines)
+
+def OutputCSV(telegrams):
+    lines = []
+    lines.append('Type, From, To, Date, Message')
+    for telegram in sorted(telegrams, key=lambda x: x.timestamp):
+        lines.append(telegram.MakeCSV())
+    return '\n'.join(lines)
+        
